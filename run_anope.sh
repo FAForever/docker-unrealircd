07426a87cd1b74a1476b8228a692c04248f1d4a2
Lukas Erlacher <l.erlacher@googlemail.com>
2017-02-05 17:37:39 +0100

    Source secrets from env
    Takes services and mysql auth data from environment and replaces them in
    config file before starting

--- run_anope.sh
+++ run_anope.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+#
+# Sets up config file and runs anope in foreground
+#
+
+# services.conf.tpl contains placeholders for secrets
+cp /home/unreal/services/conf/services.conf.tpl /home/unreal/services/conf/services.conf
+# env vars sourced from env file
+sed -i s"/%SECRET_IRC_SERVICE_PASSWORD%/${SECRET_IRC_SERVICE_PASSWORD}/" /home/unreal/unrealircd/conf/unrealircd.conf
+sed -i s"/%SECRET_IRC_DB_USER%/${SECRET_IRC_DB_USER}/" /home/unreal/unrealircd/conf/unrealircd.conf
+sed -i s"/%SECRET_IRC_DB_PASSWORD%/${SECRET_IRC_DB_PASSWORD}/" /home/unreal/unrealircd/conf/unrealircd.conf
+
 /home/unreal/unrealircd/services/bin/services -nofork -debug
