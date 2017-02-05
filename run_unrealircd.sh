525cec72fe58a20848fb942f95b3ef2b3124556a
Lukas Erlacher <l.erlacher@googlemail.com>
2017-02-05 17:36:35 +0100

    Source secrets from env
    Sources passwords and keys from environment

--- run_unrealircd.sh
+++ run_unrealircd.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 #
-# Starts unrealircd and loops as long as it runs
+# Sets up config file, starts unrealircd and loops as long as it runs
 #
+
+# unrealircd.conf.tpl contains placeholders for secrets
+cp /home/unreal/unrealircd/conf/unrealircd.conf.tpl /home/unreal/unrealircd/conf/unrealircd.conf
+# env vars sourced from env file
+sed -i s"/%SECRET_IRC_OPER_PASSWORD%/${SECRET_IRC_OPER_PASSWORD}/" /home/unreal/unrealircd/conf/unrealircd.conf
+sed -i s"/%SECRET_IRC_SERVICE_PASSWORD%/${SECRET_IRC_SERVICE_PASSWORD}/" /home/unreal/unrealircd/conf/unrealircd.conf
+sed -i s"/%SECRET_IRC_CLOAK_KEY1%/${SECRET_IRC_CLOAK_KEY1}/" /home/unreal/unrealircd/conf/unrealircd.conf
+sed -i s"/%SECRET_IRC_CLOAK_KEY2%/${SECRET_IRC_CLOAK_KEY2}/" /home/unreal/unrealircd/conf/unrealircd.conf
+sed -i s"/%SECRET_IRC_CLOAK_KEY3%/${SECRET_IRC_CLOAK_KEY3}/" /home/unreal/unrealircd/conf/unrealircd.conf
 
 /home/unreal/unrealircd/unrealircd start
 
